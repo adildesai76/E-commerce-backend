649744f44ebda9b0c476cbe60e10df7856bdfa09
ad <adildesai1707@gmail.com>
2026-07-28 02:00:50 +0530

    render

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -102,7 +102,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge:
         user.role === "admin"
           ? 2 * 60 * 60 * 1000 // 2 hours for admin
